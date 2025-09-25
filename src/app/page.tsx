fb465c10edcdb792ba9ae270d09365881e925a8b
Anuxragg <palanurag208@gmail.com>
2025-09-25 14:44:33 +0530

    final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import WhychooseUs from "./components/WhychooseUs";
 import MusicSchoolTestimonial from "./components/TestimonialCards";
 import UpcomingWebinars from "./components/ui/UpcomingWebinars";
 import Instructors from "./components/Instructors";
+import Footer from "./components/Footer";
 
 
 export default function Home() {
@@ -16,6 +17,7 @@ export default function Home() {
    <MusicSchoolTestimonial/>
    <UpcomingWebinars/>
    <Instructors/>
+   <Footer/>
    </main>
   );
 }
